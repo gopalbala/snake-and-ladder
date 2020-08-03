8ce8596a3a206e71bd9e97d1fd3266a6600e17b1
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-03 23:56:05 +0530

    added Snake and Ladder classes

---
+++ src/main/java/com/gb/snakeladder/model/Ladder.java
@@ -0,0 +1,11 @@
+package com.gb.snakeladder.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Ladder {
+    private int start;
+    private int end;
+}
---
+++ src/main/java/com/gb/snakeladder/model/Snake.java
@@ -0,0 +1,11 @@
+package com.gb.snakeladder.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Snake {
+    private int head;
+    private int tail;
+}
