30c58790261cb91ded8f93329d0aaffd5062fe6c
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-04 02:56:56 +0530

    updated main with driver code

--- src/main/java/com/gb/snakeladder/SnakeLadderApplication.java
+++ src/main/java/com/gb/snakeladder/SnakeLadderApplication.java
@@ -1,7 +1,29 @@
 package com.gb.snakeladder;
 
+import com.gb.snakeladder.model.Game;
+import com.gb.snakeladder.model.Player;
+
+import java.util.Scanner;
+
 public class SnakeLadderApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter board Size");
+        int boardSize = scanner.nextInt();
+        System.out.println("Enter number of players");
+        int numberOfPlayers = scanner.nextInt();
+        System.out.println("Enter number of snakes");
+        int numSnakes = scanner.nextInt();
+        System.out.println("Enter number of ladders");
+        int numLadders = scanner.nextInt();
 
+        Game game = new Game(numLadders, numSnakes, boardSize);
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.println("Enter player name");
+            String pName = scanner.next();
+            Player player = new Player(pName);
+            game.addPlayer(player);
+        }
+        game.playGame();
     }
 }
