47fe54c4b6143b141bb1801a472accdbde43137a
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-08-04 01:18:23 +0530

    play method

--- src/main/java/com/gb/snakeladder/model/Game.java
+++ src/main/java/com/gb/snakeladder/model/Game.java
@@ -10,20 +10,22 @@ public class Game {
     private int numberOfSnakes;
     private int numberOfLadders;
 
-    private List<Player> players;
+    private Queue<Player> players;
     private List<Snake> snakes;
     private List<Ladder> ladders;
 
     private Board board;
+    private Dice dice;
 
     public Game(int numberOfLadders, int numberOfSnakes,
                 int boardSize) {
         this.numberOfLadders = numberOfLadders;
         this.numberOfSnakes = numberOfSnakes;
-        this.players = new ArrayList<>();
+        this.players = new ArrayDeque<>();
         snakes = new ArrayList<>(numberOfSnakes);
         ladders = new ArrayList<>(numberOfLadders);
         board = new Board(boardSize);
+        dice = new Dice(1, 6, 2);
         initBoard();
     }
 
@@ -33,6 +35,8 @@ public class Game {
             while (true) {
                 int snakeStart = RandomUtils.nextInt(board.getStart(), board.getSize());
                 int snakeEnd = RandomUtils.nextInt(board.getStart(), board.getSize());
+                if (snakeEnd >= snakeStart)
+                    continue;
                 String startEndPair = String.valueOf(snakeStart) + snakeEnd;
                 if (!slSet.contains(startEndPair)) {
                     Snake snake = new Snake(snakeStart, snakeEnd);
@@ -46,6 +50,8 @@ public class Game {
             while (true) {
                 int ladderStart = RandomUtils.nextInt(board.getStart(), board.getSize());
                 int ladderEnd = RandomUtils.nextInt(board.getStart(), board.getSize());
+                if (ladderEnd <= ladderStart)
+                    continue;
                 String startEndPair = String.valueOf(ladderStart) + ladderEnd;
                 if (!slSet.contains(startEndPair)) {
                     Ladder ladder = new Ladder(ladderStart, ladderEnd);
@@ -55,5 +61,35 @@ public class Game {
                 }
             }
         }
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void playGame() {
+        while (players.size() >= 2) {
+            Player player = players.poll();
+            int val = dice.roll();
+            int newPosition = player.getPosition() + val;
+            player.setPosition(getNewPosition(newPosition));
+            if (player.getPosition() >= board.getEnd())
+                player.setWon(true);
+            else
+                players.offer(player);
+        }
+    }
+
+    private int getNewPosition(int newPosition) {
+        for (Snake snake : snakes) {
+            if (snake.getHead() == newPosition)
+                return snake.getTail();
+        }
+        for (Ladder ladder : ladders) {
+            if (ladder.getStart() == newPosition) {
+                return ladder.getEnd();
+            }
+        }
+        return newPosition;
     }
 }
